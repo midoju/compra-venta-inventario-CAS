252727c8af9c5cf065a1a1672d32ae41610df83b
midoju <lumigue88@hotmail.es>
2021-06-26 16:56:16 -0500

    Settings basedatos

--- gentelella/gentelella/settings.py
+++ gentelella/gentelella/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+from django import conf
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -80,15 +82,19 @@ WSGI_APPLICATION = 'gentelella.wsgi.application'
 import dj_database_url
 from decouple import config
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'centro_acopio',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': 3306,
-    }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'centro_acopio',
+#        'USER': 'root',
+#        'PASSWORD': '',
+#        'HOST': 'localhost',
+#        'PORT': 3306,
+#    }
+#}
+DATATABSES = {
+    'default': dj_database_url.config(default=config('CLEARDB_DATABASE_URL')
+     )
 }
 
 
