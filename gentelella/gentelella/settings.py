b12b3075fbca06993b90e880ffd7d73477895d62
midoju <lumigue88@hotmail.es>
2021-06-26 17:07:40 -0500

    actualizacion settings

--- gentelella/gentelella/settings.py
+++ gentelella/gentelella/settings.py
@@ -139,7 +139,10 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 LOGIN_REDIRECT_URL = 'inicio'
 LOGOUT_REDIRECT_URL = 'login'
